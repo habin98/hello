455eb43746043115d93f701ab00c7ffe84239f32
Admin <habinarshia@gmail.com>
2022-05-02 09:33:36 +0530

    hai Everyone

---
+++ demo2/src/main/java/tm1/demo2/Hello.java
@@ -0,0 +1,12 @@
+package tm1.demo2;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Hello");
+		System.out.println("World");
+
+	}
+
+}
